5c7233d3fabf72bb8d5c2063b9da64ad687cd440
pkmitl205 <58030205@kmitl.ac.th>
2020-02-06 23:09:14 +0700

    สร้าง View และการกำหนดเส้นทางด้วย urls.py

--- todoProject/todoApp/views.py
+++ todoProject/todoApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello World!, you are at the todoApp index page.")
+
+def about(request):
+    return HttpResponse("This is the about page of todoApp")
