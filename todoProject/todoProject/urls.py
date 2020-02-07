ae7b336ea1aa18db037a11e59f9dc76050b69fa6
pkmitl205 <58030205@kmitl.ac.th>
2020-02-07 23:07:43 +0700

    สร้าง Navigation bar โดยใช้ Bootstrap

--- todoProject/todoProject/urls.py
+++ todoProject/todoProject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todoApp/', include('todoApp.urls')),
+    path('', include('todoApp.urls')),
 ]
