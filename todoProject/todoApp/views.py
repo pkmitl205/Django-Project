4b55c6ef1dd75ff1dd3656d62ef924afc7fcd1cc
pkmitl205 <58030205@kmitl.ac.th>
2020-02-07 23:28:00 +0700

    การสร้าง Links ด้วย Django link

--- todoProject/todoApp/views.py
+++ todoProject/todoApp/views.py
@@ -8,3 +8,9 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def contact(request):
+    first_name = "Phakon"
+    last_name = "Rujireksareekul"
+    email = "58030205@kmitl.ac.th"
+    return render(request, 'contact.html', {"first_name" : first_name, "last_name" : last_name, "email" : email })
