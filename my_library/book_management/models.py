780a685401115888f9a2fb640cd9a91da18400f5
pkmitl205 <58030205@kmitl.ac.th>
2020-02-20 23:48:16 +0700

    create models and register in admin site

--- my_library/book_management/models.py
+++ my_library/book_management/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    email = models.EmailField()
+
+class Book(models.Model):
+    isbn = models.CharField(
+        max_length = 200, 
+        primary_key=True, 
+        unique=True, 
+        null=False) 
+    title = models.CharField(max_length = 200)
+    author = models.ManyToManyField(Author)
+    publisher = models.CharField(max_length = 200)
+    price = models.FloatField(default = 0)
